a5e9629476e608ed8ec295116bba02815355e577
fatima-zahraramdani <fatimazahra.ramdani@usmba.ac.ma>
2022-08-17 18:37:26 +0100

    HATEOAS and content-negotiation added

--- RESTful-web-service/src/main/java/com/example/RESTfulwebservice/ResTfulWebServiceApplication.java
+++ RESTful-web-service/src/main/java/com/example/RESTfulwebservice/ResTfulWebServiceApplication.java
@@ -1,13 +1,35 @@
 package com.example.RESTfulwebservice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class ResTfulWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResTfulWebServiceApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		
+		localResolver.setDefaultLocale(Locale.US);
+		
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource; 
 	}
 
 }
--- RESTful-web-service/src/main/java/com/example/RESTfulwebservice/user/UserResource.java
+++ RESTful-web-service/src/main/java/com/example/RESTfulwebservice/user/UserResource.java
@@ -1,18 +1,28 @@
 package com.example.RESTfulwebservice.user;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 //import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 public class UserResource {
 	
@@ -20,17 +30,25 @@ public class UserResource {
 	@Autowired
 	private UserDaoService service;
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping("/users")
 	public List<User> retrievAllUsers(){
 		return service.findAll();
 	}
 	
 	@GetMapping("/user/{id}")
-	public User retrievUser(@PathVariable long id){
+	public EntityModel<User> retrievUser(@PathVariable long id){
 		User user = service.findOne(id);
 		if (user == null)
 				throw new UserNotFoundException("User Not Found !");
-		return user;
+		
+		EntityModel<User> entitymodel = EntityModel.of(user);
+		WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).retrievAllUsers());
+		entitymodel.add(link.withRel("all-users"));
+		
+		return entitymodel;
 	}
 	
 	@PostMapping("/user")
@@ -48,6 +66,11 @@ public class UserResource {
 				throw new UserNotFoundException("User Not Found !");
 		
 		//return user;
+	}
+	
+	@GetMapping("/home")
+	public String HelloWorld(@RequestHeader(name="Accept-Language", required=false) Locale local) {
+		return messageSource.getMessage("welcome.text.message", null,local);
 	}
 	
 	
